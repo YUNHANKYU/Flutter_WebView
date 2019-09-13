cd2f12772c70cb420ef827751633193841ed1b7b
YUNHANKYU <bhgt4563@gmail.com>
2019-09-14 02:01:49 +0900

    [UPDATE] you can get current URL

--- lib/web_view_container.dart
+++ lib/web_view_container.dart
@@ -3,18 +3,21 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewContainer extends StatefulWidget {
   final url;
+  final controller;
 
-  WebViewContainer(this.url);
+  WebViewContainer(this.url, this.controller);
 
   @override
-  _WebViewContainerState createState() => _WebViewContainerState(this.url);
+  _WebViewContainerState createState() =>
+      _WebViewContainerState(this.url, this.controller);
 }
 
 class _WebViewContainerState extends State<WebViewContainer> {
   var _url;
   final _key = UniqueKey();
+  WebViewController _controller;
 
-  _WebViewContainerState(this._url);
+  _WebViewContainerState(this._url, this._controller);
 
   @override
   Widget build(BuildContext context) {
@@ -27,10 +30,26 @@ class _WebViewContainerState extends State<WebViewContainer> {
               key: _key,
               javascriptMode: JavascriptMode.unrestricted,
               initialUrl: _url,
+              onWebViewCreated: (WebViewController webViewController) async {
+                _controller = webViewController;
+                var dd = await _controller.currentUrl();
+                print("======== $dd ========");
+              },
             ),
           )
         ],
-      )
+      ),
+      floatingActionButton: _bookmarkButton(context, _controller),
+    );
+  }
+
+  _bookmarkButton(BuildContext context ,WebViewController _controller) {
+    return FloatingActionButton(
+      onPressed: () async {
+        var url = await _controller.currentUrl();
+        print("@@@@@@@@@ $url @@@@@@@@@");
+      },
+      child: Icon(Icons.favorite),
     );
   }
 }
